ba770b94db37d35244bba4b06bf85bdc8a34d7af
Yash Kumar Verma <yk.verma2000@gmail.com>
2021-02-13 22:11:54 +0530

    test(controller): ensure all endpoints defiend

--- src/user/user.controller.spec.ts
+++ src/user/user.controller.spec.ts
@@ -1,26 +1,71 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { User, UserSchema } from './user.schema';
 import { closeInMongodConnection, rootMongooseTestModule } from '../../test-utils/MongooseTestModule';
 
 import { MongooseModule } from '@nestjs/mongoose';
 import { UserController } from './user.controller';
-import { UserSchema } from './user.schema';
 import { UserService } from './user.service';
 
-describe('SquidController', () => {
+describe('UserController', () => {
   let controller: UserController;
+  let service: UserService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       imports: [rootMongooseTestModule(), MongooseModule.forFeature([{ name: 'User', schema: UserSchema }])],
       controllers: [UserController],
-      providers: [UserService],
+      providers: [
+        {
+          provide: UserService,
+          useFactory: () => ({
+            login: jest.fn(() => true),
+            create: jest.fn(() => true),
+            pushApplicationIntoUserParticipantList: jest.fn(() => true),
+            findAll: jest.fn(() => true),
+            findOneById: jest.fn(() => true),
+            update: jest.fn(() => true),
+            remove: jest.fn(() => true),
+          }),
+        },
+      ],
     }).compile();
 
     controller = module.get<UserController>(UserController);
+    service: module.get<UserService>(UserService);
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  describe('.create()', () => {
+    it('should be defined', () => {
+      expect(controller.create).toBeDefined();
+    });
+  });
+
+  describe('.findAll()', () => {
+    it('should be defined', () => {
+      expect(controller.findAll).toBeDefined();
+    });
+  });
+
+  describe('.findOne()', () => {
+    it('should be defined', () => {
+      expect(controller.findOne).toBeDefined();
+    });
+  });
+
+  describe('.update()', () => {
+    it('should be defined', () => {
+      expect(controller.update).toBeDefined();
+    });
+  });
+
+  describe('.remove()', () => {
+    it('should be defined', () => {
+      expect(controller.remove).toBeDefined();
+    });
   });
 
   afterAll(async () => {
