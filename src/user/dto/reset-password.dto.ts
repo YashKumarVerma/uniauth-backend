9ce87b31d3ba2054edafa4023185af5921011da0
HARSH PANDEY <54094722+MASTERCHIEF011@users.noreply.github.com>
2021-02-15 09:58:50 +0530

    feat(passwd): implemented reset (#57)

---
+++ src/user/dto/reset-password.dto.ts
@@ -0,0 +1,31 @@
+import { IsNotEmpty, MinLength, } from 'class-validator';
+
+import { ApiProperty } from '@nestjs/swagger';
+
+/**
+ * **Reset Password DTO**
+ *
+ * [[ResetPasswordDto]] is responsible for handling input and updating the same 
+ * 
+ *
+ * @category User
+ */
+export class ResetPasswordDto {
+ 
+
+  /** password of user */
+  @ApiProperty({ description: 'password of user', example: '12345678' })
+  @MinLength(8)
+  @IsNotEmpty()
+  password_1: string;
+
+  @ApiProperty({ description: 'password of user', example: '12345678' })
+  @MinLength(8)
+  @IsNotEmpty()
+  password_2: string;
+}
+
+export class ResetPasswordDtoWithCaptcha extends ResetPasswordDto {
+  @IsNotEmpty()
+  token: string;
+}
