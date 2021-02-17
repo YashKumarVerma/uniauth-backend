5e3b508d45a7cb6eccbb02be0994ba9a92c8ff04
Tanay Raj <tanay.raj76@gmail.com>
2021-02-18 00:38:20 +0530

    test(dashboard): controller setup (#84)

--- src/dashboard/dashboard.controller.spec.ts
+++ src/dashboard/dashboard.controller.spec.ts
@@ -1,6 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { DashboardController } from './dashboard.controller';
-import { DashboardService } from './dashboard.service';
+import { UserService } from '../user/user.service';
+import {ApplicationService} from '../application/application.service'
 
 describe('DashboardController', () => {
   let controller: DashboardController;
@@ -8,13 +9,60 @@ describe('DashboardController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [DashboardController],
-      providers: [DashboardService],
+      providers: [
+        {
+          provide: UserService,
+          useFactory: () => ({
+            login: jest.fn(() => true),
+            create: jest.fn(() => true),
+            pushApplicationIntoUserParticipantList: jest.fn(() => true),
+            findAll: jest.fn(() => true),
+            findOneById: jest.fn(() => true),
+            update: jest.fn(() => true),
+            remove: jest.fn(() => true),
+          }),
+        },
+       { 
+          provide: ApplicationService,
+          useFactory: () => ({
+            create: jest.fn(() => true),
+            findAll: jest.fn(() => true),
+            findOneById: jest.fn(() => true),
+            findAllByOwner: jest.fn(() => true),
+            findAllByParticipant: jest.fn(() => true),
+            pushUserIntoApplicationParticipantList: jest.fn(() => true),
+            findOneByIdAndSecret: jest.fn(() => true),
+          })
+        }
+      ],
     }).compile();
 
     controller = module.get<DashboardController>(DashboardController);
+    service: module.get<UserService>(UserService);
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
   });
+  describe('.showDashboard()', () => {
+    it('should be defined', () => {
+      expect(controller.showDashboard).toBeDefined();
+    });
+  });
+  describe('.showProfile()',() => {
+    it('should be defined',() => {
+      expect(controller.showProfile).toBeDefined();
+    });
+  });
+  describe('.showData()',() => {
+    it('should be defined',() => {
+      expect(controller.showData).toBeDefined();
+    });
+  });
+  describe('.showDev()',() => {
+    it('should be defined',() => {
+      expect(controller.showDev).toBeDefined();
+    });
+  });
+
 });
