b6630b66402ece18e2fe97246af0b8f5c4234551
HARSH PANDEY <54094722+MASTERCHIEF011@users.noreply.github.com>
2021-02-18 23:37:31 +0530

    test(application): add tests for controller (#93)

--- src/application/application.controller.spec.ts
+++ src/application/application.controller.spec.ts
@@ -1,4 +1,5 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { closeInMongodConnection } from '../../test-utils/MongooseTestModule';
 import { ApplicationController } from './application.controller';
 import { ApplicationService } from './application.service';
 
@@ -8,7 +9,17 @@ describe('ApplicationController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [ApplicationController],
-      providers: [ApplicationService],
+      providers: [
+        ApplicationService,
+        {
+          provide: ApplicationService,
+          useFactory: () => ({
+            create: jest.fn(() => true),
+            findAll: jest.fn(() => true),
+            findOneById: jest.fn(() => true),
+          }),
+        },
+      ],
     }).compile();
 
     controller = module.get<ApplicationController>(ApplicationController);
@@ -16,5 +27,33 @@ describe('ApplicationController', () => {
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  describe('.create()', () => {
+    it('should be defined', () => {
+      expect(controller.create).toBeDefined();
+    });
+  });
+
+  describe('.findAll()', () => {
+    it('should be defined', () => {
+      expect(controller.findAll).toBeDefined();
+    });
+  });
+
+  describe('.findOne()', () => {
+    it('should be defined', () => {
+      expect(controller.findOne).toBeDefined();
+    });
+  });
+
+  describe('.remove()', () => {
+    it('should be defined', () => {
+      expect(controller.remove).toBeDefined();
+    });
+  });
+
+  afterAll(async () => {
+    await closeInMongodConnection();
   });
 });
