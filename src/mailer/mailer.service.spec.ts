ca55aeb9125cef82000efba218a1038e1a7bae68
HARSH PANDEY <54094722+MASTERCHIEF011@users.noreply.github.com>
2021-02-18 22:01:03 +0530

    feat(mailer): add tests for service (#85)

--- src/mailer/mailer.service.spec.ts
+++ src/mailer/mailer.service.spec.ts
@@ -1,18 +1,84 @@
+import { JwtModule } from '@nestjs/jwt';
+import { getModelToken, MongooseModule } from '@nestjs/mongoose';
 import { Test, TestingModule } from '@nestjs/testing';
+import { Model } from 'mongoose';
+import { User, UserDocument, UserSchema } from '../user/user.schema';
+import { rootMongooseTestModule } from '../../test-utils/MongooseTestModule';
+import { confirmEmailTokenConstants } from './constants/confirmEmailToken.constants';
 import { MailerService } from './mailer.service';
+import * as config from 'config';
+import { UserService } from '../user/user.service';
+
+const mockUser = (mock?: Partial<User>): Partial<UserDocument> => ({
+  name: 'some user',
+  batch: '19',
+  branch: 'BCE',
+  personalEmail: 'someone@example.com',
+  collegeEmail: 'someoe@edu.in',
+});
 
 describe('MailerService', () => {
+  let testingModule: TestingModule;
   let service: MailerService;
+  let model: Model<UserDocument>;
 
   beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      providers: [MailerService],
+    testingModule = await Test.createTestingModule({
+      imports: [
+        rootMongooseTestModule(),
+        MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
+        JwtModule.register({
+          secret: confirmEmailTokenConstants.secret,
+          signOptions: { expiresIn: confirmEmailTokenConstants.expiresIn },
+        }),
+      ],
+      providers: [
+        MailerService,
+        UserService,
+        {
+          provide: getModelToken(User.name),
+          useValue: {
+            findByIdAndUpdate: jest.fn().mockResolvedValue(mockUser()),
+          },
+        },
+      ],
     }).compile();
 
-    service = module.get<MailerService>(MailerService);
+    service = testingModule.get<MailerService>(MailerService);
+    model = testingModule.get<Model<UserDocument>>(getModelToken(User.name));
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  describe('.generateJwt()', () => {
+    it('should be defined', () => {
+      expect(service.generateJwt).toBeDefined();
+    });
+  });
+
+  describe('.checkVerificationToken()', () => {
+    it('should be defined', () => {
+      expect(service.checkVerificationToken).toBeDefined();
+    });
+  });
+
+  describe('.checkPasswordResetToken()', () => {
+    it('should be defined', () => {
+      expect(service.checkPasswordResetToken).toBeDefined();
+    });
+  });
+
+  describe('.sendEmail()', () => {
+    it('should be defined', () => {
+      expect(service.sendEmail).toBeDefined();
+    });
+  });
+
+  describe('.sendPasswordResetLink()', () => {
+    it('should be defined', () => {
+      expect(service.sendPasswordResetLink).toBeDefined();
+    });
   });
 });
