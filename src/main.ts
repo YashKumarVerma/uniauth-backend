2ffe8c0a40341f834332e1cf5940c40ef5840a90
KARANDEEP SINGH <50829119+kdsinghcoder@users.noreply.github.com>
2021-02-27 10:40:44 +0530

    refactor(views): add partials to avoid code duplication (#121)

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import * as config from 'config';
 import * as cookieParser from 'cookie-parser';
 import * as helmet from 'helmet';
 import * as rateLimit from 'express-rate-limit';
-
+import * as hbs from 'hbs';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 import { AppModule } from './app.module';
@@ -23,7 +23,10 @@ async function bootstrap() {
   /** configuring public and views directory */
   app.useStaticAssets(join(__dirname, '../..', 'public'));
   app.setBaseViewsDir(join(__dirname, '../..', 'views'));
+  hbs.registerPartials(join(__dirname, '../../views', 'partials'));
+
   app.setViewEngine('hbs');
+  console.log(__dirname);
 
   /** configuring swaggerUI */
   const options = new DocumentBuilder()
